8f0017d84bf01c62b132e49fad8f3bdfa1f153b5
greg mathias <mathiasgreg@gmail.com>
2020-10-27 12:47:10 -0400

    update json et al

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -17,14 +17,8 @@ class Footer extends Component {
            <ul className="social-links">
               {networks}
            </ul>
-
-           <ul>
-            <li>Modified with love by the Clever Programmer team ♥️</li>
-           </ul>
-
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Greg Mathias</li>
            </ul>
 
         </div>
